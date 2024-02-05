bbcdda5e96b905bfa2145809011eeb56dcf2ac8b
Urban Strajnar <urban.strajnar@trickytribe.com>
2024-02-05 18:26:12 +0100

    initial setup

---
+++ Assets/UrbiEtUrbi/Scripts/Runtime/ColorLookup2.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+[Serializable, VolumeComponentMenuForRenderPipeline("Custom/CustomEffectComponent", typeof(UniversalRenderPipeline))]
+public class ColorLookup4 : VolumeComponent, IPostProcessComponent
+{
+
+    public ClampedFloatParameter intensity = new ClampedFloatParameter(value: 0, min: 0, max: 1, overrideState: true);
+    // A color that is constant even when the weight changes
+    public NoInterpColorParameter fromColor1 = new NoInterpColorParameter(Color.black);
+    public NoInterpColorParameter fromColor2 = new NoInterpColorParameter(Color.black);
+
+    public NoInterpColorParameter toColor1 = new NoInterpColorParameter(Color.black);
+    public NoInterpColorParameter toColor2 = new NoInterpColorParameter(Color.black);
+
+
+    public bool IsActive()
+    {
+        return intensity.value > 0;
+      
+    }
+
+    public bool IsTileCompatible()
+    {
+        return true;
+    }
+}
